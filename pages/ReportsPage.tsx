1d641598502dc38e7d0b66e713d7ec7f0e25d09e
Islam1010101 <lyv48243@gmail.com>
2026-02-24 21:42:16 +0400

    Update ReportsPage.tsx

--- pages/ReportsPage.tsx
+++ pages/ReportsPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useLanguage } from '../App';
 import { collection, getDocs, query, orderBy } from 'firebase/firestore';
-import { db } from '../../utils/firebase
+import { db } from '../../utils/firebase';
 
 const translations = {
     ar: {
