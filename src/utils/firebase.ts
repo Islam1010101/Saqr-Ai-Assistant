730730e7b779f306853c47c3af32a66634113118
Islam1010101 <lyv48243@gmail.com>
2026-02-24 21:48:40 +0400

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,7 +3,6 @@ import { getFirestore } from "firebase/firestore";
 
 // 🛑 شيل دول وحط المفاتيح بتاعتك اللي جبتها من موقع فايربيز
 const firebaseConfig = {
-const firebaseConfig = {
   apiKey: "AIzaSyCIlWYb6S2ViVKs12jYPzbiFdYQit1KYCM",
   authDomain: "efips-smart-portal.firebaseapp.com",
   projectId: "efips-smart-portal",
