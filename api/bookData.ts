4bc01d7c1c72a69386ee05b11cbe113e904554f5
Islam1010101 <lyv48243@gmail.com>
2026-06-05 07:09:53 +0400

    Update bookData.ts

--- api/bookData.ts
+++ api/bookData.ts
@@ -3333,7 +3333,7 @@ const rawBookData = [
 { "title": "Soudium Counter", "author": "Louis Weber", "shelf": 10, "row": 1 },  
 { "title": "Zoo Frankfurt", "author": "R. Faust", "shelf": 10, "row": 1 },
 { "title": "At The Love Corner Of Love And Heartache", "author": "Curtiss Ann Matlock", "shelf": 19, "row": 6 },
-{ "title": "New Webster's Dictionary And Roget's Thesaurus", "author": "Book Essentials Inc", "shelf": 5, "row": 5 }
+{ "title": "New Webster's Dictionary And Roget's Thesaurus", "author": "Book Essentials Inc", "shelf": 6, "row": 3 }
 ];
 
 // 3. دالة معالجة البيانات
